ee33c0146b088efbe924d3f28fe82f6e5d3e318f
bdruhul <wp.dev.ruhul@gmail.com>
2025-02-02 06:52:43 +0600

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,17 @@
+/*--------------- Custom color based client requirements ------------*/
+
+module.exports = {
+    content: [
+      './src/**/*.{html,js}',
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1D4ED8',
+          secondary: '#9333EA',
+        },
+      },
+    },
+    plugins: [],
+  }
+/*--------------- Custom color based client requirements ------------*/
